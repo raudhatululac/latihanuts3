52d7f667e96ce73141f79e3739c9bc02b26ca03a
raudhatululac <32457062+raudhatululac@users.noreply.github.com>
2017-11-14 23:56:53 +0700

    Add files via upload

---
+++ PBO12_10116598_LATIHANUTS3/src/latihanuts3/LatihanUTS3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanuts3;
+
+/**
+ *
+ * @author Raudhatul Ula Camalia (10116598) PBO12
+ */
+public class LatihanUTS3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
